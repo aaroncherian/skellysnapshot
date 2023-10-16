dde3d105be612ded6230cc56f413f5dbdeca764d
aaroncherian <aaron.cherian2@gmail.com>
2023-10-16 12:18:27 -0400

    getting 3d reconstruction working with camera snapshot input

--- skellysnapshot/camera_to_pose_test.py
+++ skellysnapshot/camera_to_pose_test.py
@@ -7,6 +7,7 @@ import cv2
 from skellysnapshot.cameras.camera_test import main
 from skellysnapshot.constants import TaskNames
 from skellysnapshot.task_worker_thread import TaskWorkerThread
+from calibration.anipose_object_loader import load_anipose_calibration_toml_from_path
 
 def initialize_gui():
     app = QApplication(sys.argv)
@@ -60,12 +61,12 @@ class MyClass:
 
     def run(self, snapshot, calibration_toml_path):
         # Load the calibration object
-        # anipose_calibration_object = load_anipose_calibration_toml_from_path(calibration_toml_path)
-        anipose_calibration_object = None
+        anipose_calibration_object = load_anipose_calibration_toml_from_path(calibration_toml_path)
+        # anipose_calibration_object = None
         task_worker_thread = TaskWorkerThread(
             snapshot=snapshot,
             anipose_calibration_object=anipose_calibration_object,
-            task_queue=[TaskNames.TASK_RUN_MEDIAPIPE],
+            task_queue=[TaskNames.TASK_RUN_MEDIAPIPE, TaskNames.TASK_RUN_3D_RECONSTRUCTION],
             task_running_callback=None,
             task_completed_callback=None,
             all_tasks_completed_callback=self.handle_all_tasks_completed
@@ -79,24 +80,27 @@ class MyClass:
 
 
 
-def my_snapshot_callback(snapshot, tab_widget):
+def my_snapshot_callback(snapshot, path_to_calibration_toml, tab_widget):
     my_class = MyClass()
-    my_class.run(snapshot, [])
+    my_class.run(snapshot, path_to_calibration_toml)
 
     # Assume snapshot_data is available here, replace with actual data
     snapshot_data = my_class.snapshot2d_data.annotated_images
     add_snapshot_tab(tab_widget, snapshot_data)
 
 if __name__ == "__main__":
+    from pathlib import Path
 
     settings_dict = {
         'timer':4
     }
 
+    path_to_calibration_toml = Path(r"C:\Users\aaron\FreeMocap_Data\recording_sessions\session_2023-10-16_11_53_42\recording_11_59_27_gmt-4_calibration\recording_11_59_27_gmt-4_calibration_camera_calibration.toml")
+
     app, window, tab_widget = initialize_gui()
 
     def snapshot_callback_wrapper(snapshot):
-        my_snapshot_callback(snapshot, tab_widget)
+        my_snapshot_callback(snapshot,path_to_calibration_toml ,tab_widget)
 
     main(snapshot_callback=snapshot_callback_wrapper,settings_dict=settings_dict)
 
