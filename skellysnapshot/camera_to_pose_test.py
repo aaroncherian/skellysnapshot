4c07f42b948f1ea3d94581b991caa6b0ffc13c1b
aaroncherian <aaron.cherian2@gmail.com>
2023-10-16 11:34:35 -0400

    prelim pyqt6 gui

--- skellysnapshot/camera_to_pose_test.py
+++ skellysnapshot/camera_to_pose_test.py
@@ -1,8 +1,49 @@
+from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtWidgets import QApplication, QTabWidget, QMainWindow, QLabel, QVBoxLayout, QWidget
+import sys
 import cv2
 
 from skellysnapshot.cameras.camera_test import main
 from skellysnapshot.constants import TaskNames
 from skellysnapshot.task_worker_thread import TaskWorkerThread
+
+def initialize_gui():
+    app = QApplication(sys.argv)
+
+    window = QMainWindow()
+    window.setWindowTitle("Snapshot Viewer")
+    window.setGeometry(200, 200, 800, 600)
+
+    tab_widget = QTabWidget()
+
+    window.setCentralWidget(tab_widget)
+    window.show()
+
+    return app, window, tab_widget
+
+# Function to add a new tab with the snapshot images
+def add_snapshot_tab(tab_widget, snapshot_images):
+    # Create a new tab widget
+    new_tab = QWidget()
+    layout = QVBoxLayout()
+
+    for name, image in snapshot_images.items():
+        # Convert the image to be compatible with PyQt6
+        height, width, channel = image.shape
+        bytes_per_line = 3 * width
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        qt_image = QImage(image.data, width, height, bytes_per_line, QImage.Format.Format_RGB888)
+        pixmap = QPixmap.fromImage(qt_image)
+
+        # Create a QLabel to hold the image
+        label = QLabel()
+        label.setPixmap(pixmap)
+        layout.addWidget(label)
+
+    # Add the layout to the tab
+    new_tab.setLayout(layout)
+    tab_widget.addTab(new_tab, f"Snapshot {tab_widget.count() + 1}")
 
 
 class MyClass:
@@ -10,12 +51,9 @@ class MyClass:
         self.snapshot3d_data = None
 
     def handle_all_tasks_completed(self, task_results: dict):
-        snapshot2d_data = task_results[TaskNames.TASK_RUN_MEDIAPIPE]['result']
+        self.snapshot2d_data = task_results[TaskNames.TASK_RUN_MEDIAPIPE]['result']
         # self.snapshot3d_data = task_results[TaskNames.TASK_RUN_3D_RECONSTRUCTION]['result']
         # print(snapshot2d_data.data_2d_camera_frame_marker_dimension.shape)
-
-        for name, image in snapshot2d_data.annotated_images.items():
-            print(name)
 
         # print(self.snapshot3d_data.data_3d_camera_frame_marker_dimension.shape)
 
@@ -40,10 +78,20 @@ class MyClass:
 
 
 
-def my_snapshot_callback(snapshot):
-    print('starting callback')
+def my_snapshot_callback(snapshot, tab_widget):
     my_class = MyClass()
     my_class.run(snapshot, [])
 
+    # Assume snapshot_data is available here, replace with actual data
+    snapshot_data = my_class.snapshot2d_data.annotated_images
+    add_snapshot_tab(tab_widget, snapshot_data)
+
 if __name__ == "__main__":
-    main(snapshot_callback=my_snapshot_callback)
+    app, window, tab_widget = initialize_gui()
+
+    def snapshot_callback_wrapper(snapshot):
+        my_snapshot_callback(snapshot, tab_widget)
+
+    main(snapshot_callback=snapshot_callback_wrapper)
+
+    sys.exit(app.exec())
