37d5581f5ead1a2ca447987857df3eecf140563e
aaroncherian <aaron.cherian2@gmail.com>
2023-11-08 09:37:40 -0500

    Update calibration_menu.py

--- skellysnapshot/gui/widgets/calibration_menu.py
+++ skellysnapshot/gui/widgets/calibration_menu.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import pyqtSignal, QObject
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog, QSizePolicy, QGroupBox
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog, QSizePolicy, QGroupBox, QSpacerItem
 
 from skellysnapshot.calibration.freemocap_anipose import CameraGroup  # Import the type
 
@@ -30,11 +30,27 @@ class CalibrationMenu(QWidget):
         super().__init__()
         layout = QVBoxLayout()
 
+        self.add_calibration_object_label(layout)
+        self.add_calibration_toml_groupbox(layout)
+
+        spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        layout.addItem(spacer)
+        self.setLayout(layout)
+
+    def add_calibration_object_label(self, layout):
+        calibration_status_groupbox = QGroupBox("Calibration Status")
+        calibration_status_groupbox.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        calibration_status_layout = QVBoxLayout()
+
         self.calibration_object_label = QLabel("Calibration Object: Not Loaded")
         self.calibration_object_label.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)  # Set the size policy to Fixed
-        layout.addWidget(self.calibration_object_label)
+        calibration_status_layout.addWidget(self.calibration_object_label)
+        
+        calibration_status_groupbox.setLayout(calibration_status_layout)
+        layout.addWidget(calibration_status_groupbox)
 
 
+    def add_calibration_toml_groupbox(self, layout):
         toml_calibration_group = QGroupBox("Load Calibration File")
         toml_cal_layout = QVBoxLayout()
         self.calibration_file_label = QLabel("Calibration File: Not Loaded")
@@ -47,7 +63,6 @@ class CalibrationMenu(QWidget):
         toml_calibration_group.setLayout(toml_cal_layout)
         layout.addWidget(toml_calibration_group)
 
-        self.setLayout(layout)
 
     def load_calibration_file(self):
         filePath, _ = QFileDialog.getOpenFileName(self, "Load Calibration File", "", "TOML Files (*.toml);;All Files (*)")
