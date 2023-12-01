66961ab5ed7ed2865c9ee38b74b57f64ee4b642d
aaroncherian <aaron.cherian2@gmail.com>
2023-12-01 14:32:04 -0500

    Update queue_manager.py

--- skellysnapshot/gui/helpers/queue_manager.py
+++ skellysnapshot/gui/helpers/queue_manager.py
@@ -6,12 +6,18 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 import logging
+import time
+
 
 class QueueManager:
     def __init__(self, num_workers):
+        start_time = time.time()
         self.task_queue = queue.Queue()
         self.num_workers = num_workers
         self.executor = ThreadPoolExecutor(max_workers=num_workers)
+        init_time = time.time() - start_time
+        logging.info(f"ThreadPoolExecutor initialization time: {init_time} seconds")
+
         self.active_tasks = set()
 
     def add_task(self, task):
@@ -27,11 +33,23 @@ class QueueManager:
 
             logging.info(f"Sending snapshot {task['id']} for processing. Queue size: {self.task_queue.qsize()}")
             processor = TaskWorkerThread(task)
+            start_time = time.time()
             future = self.executor.submit(processor.process_tasks)
+            submit_time = time.time() - start_time
+            logging.info(f"Time to submit Snapshot {task['id']}: {submit_time} seconds")
+
             self.active_tasks.add(future)
-            future.add_done_callback(lambda f: self.active_tasks.remove(f))
+            future.add_done_callback(lambda f: self.task_done_callback(f, task['id']))
+
 
             self.task_queue.task_done()
+
+    def task_done_callback(self, future, task_id):
+        if future.exception() is not None:
+            logging.error(f"Snapshot {task_id} resulted in an error: {future.exception()}")
+        else:
+            logging.info(f"Snapshot {task_id} completed")
+        self.active_tasks.remove(future)
 
     def stop(self):
         # Add a stop signal to the queue
